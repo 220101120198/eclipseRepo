8e075b7b82d5d39bbee542dee3f9f97c256ea4f4
Chandu Maharana <chandumaharana58537@gmail.com>
2024-09-16 12:40:28 +0530

    "gitRepoapplication file is added"

---
+++ gitRepo1/src/main/java/com/spring/demo/GitRepo1Application.java
@@ -0,0 +1,13 @@
+package com.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepo1Application.class, args);
+	}
+
+}
